e51041479cd0dc3c398c5c98188d50952d59c968
lordaake <lordaake@protonmail.com>
2023-08-16 18:18:07 +0200

    changed to rest api keys in js files

--- common.js
+++ common.js
@@ -1,11 +1,35 @@
-const baseURL = 'https://themlmleader.com/wp-json/wc/store/products';
+const baseURL = 'https://themlmleader.com/wp-json/wc/v3/products';
+const consumerKey = 'ck_56677970ba6b9ec1209b1e3760af60813d87d2e9';
+const consumerSecret = 'cs_1203e5e3143daf75c1266aee7ca0f8b37f5a59e6';
 
 function fetchProducts() {
-    return fetch(baseURL)
-        .then(response => response.json());
+    const url = `${baseURL}?consumer_key=${consumerKey}&consumer_secret=${consumerSecret}`;
+    
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Failed to fetch products");
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.error(error);
+            throw error;
+        });
 }
 
 function fetchSingleProduct(id) {
-    return fetch(`${baseURL}/${id}`)
-        .then(response => response.json());
+    const url = `${baseURL}/${id}?consumer_key=${consumerKey}&consumer_secret=${consumerSecret}`;
+    
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Failed to fetch product");
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.error(error);
+            throw error;
+        });
 }
--- products.js
+++ products.js
@@ -2,18 +2,45 @@
 fetchProducts()
     .then(data => {
         let productsHTML = '';
+        let featuredProduct; // Declare a variable to store the featured product
+
         data.forEach(product => {
-            productsHTML += `
-            <div class="product">
-                <img src="${product.images[0].src}" alt="${product.name}">
-                <h2>${product.name}</h2>
-                <p>${product.short_description}</p>
-                <p>Price: ${product.price_html}$</p>
-                <a href="product-detail.html?id=${product.id}">See Details</a>
-            </div>
-        `;
+            if (product.featured) {
+                // If the product is featured, store it as the featured product
+                featuredProduct = product;
+            } else {
+                // If the product is not featured, add it to the productsHTML
+                productsHTML += `
+                <div class="product">
+                    <a class="image-link" href="product-detail.html?id=${product.id}">
+                        <img src="${product.images[0].src}" alt="${product.name}">
+                    </a>
+                    <h2>${product.name}</h2>
+                    <p>${product.short_description}</p>
+                    <p>Price: ${product.price_html}$</p>
+                    <a class="text-link" href="product-detail.html?id=${product.id}">See Details</a>
+                </div>
+                `;
+            }
         });
 
+        // Display the featured product separately with a clear indication that it is featured
+        if (featuredProduct) {
+            document.getElementById('featured-product').innerHTML = `
+            <div class="featured-product">
+                <div class="featured-tag">Featured</div>
+                <a class="image-link" href="product-detail.html?id=${featuredProduct.id}">
+                    <img src="${featuredProduct.images[0].src}" alt="${featuredProduct.name}">
+                </a>
+                <h2>${featuredProduct.name}</h2>
+                <p>${featuredProduct.short_description}</p>
+                <p>Price: ${featuredProduct.price_html}$</p>
+                <a class="text-link" href="product-detail.html?id=${featuredProduct.id}">See Details</a>
+            </div>
+            `;
+        }
+
+        // Set the productsHTML to the products container
         document.getElementById('products').innerHTML = productsHTML;
     })
     .catch(error => console.error('Error fetching products:', error));
